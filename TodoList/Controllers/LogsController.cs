9fc84a1405556b3af8700b5b485fc39acffa881f
quynhhoaa <quynhhoa020305@gmail.com>
2023-03-09 18:10:23 +0700

    register, login, jwt token

---
+++ TodoList/Controllers/LogsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32.SafeHandles;
+using TodoList.DTOs;
+using TodoList.Models;
+using TodoList.Services.PasswordHash;
+using TodoList.Services.TokenGenerator;
+
+namespace TodoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly BCryptPasswordHash _bCryptPasswordHash;
+        private readonly TodoDbContext _todoDbContext;
+        private readonly AccessTokenGenerator _tokenGenerator;
+
+        public LogsController(BCryptPasswordHash bCryptPasswordHash, TodoDbContext todoDbContext, AccessTokenGenerator tokenGenerator)
+        {
+            _bCryptPasswordHash = bCryptPasswordHash;
+            _todoDbContext = todoDbContext;
+            _tokenGenerator = tokenGenerator;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register([FromBody] UserRequest userRequest)
+        {
+            var passwordHash = _bCryptPasswordHash.HashPassword(userRequest.Password);
+            user.Username = userRequest.Username;
+            user.PasswordHash = passwordHash;
+            user.Email = userRequest.Email;
+            user.Id = new Guid();
+            _todoDbContext.Add(user);
+            await _todoDbContext.SaveChangesAsync();
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] UserRequest userRequest)
+        {
+            var user = _todoDbContext.Users.SingleOrDefault(x => x.Username == userRequest.Username);
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+            /* var passwordHash = _bCryptPasswordHash.HashPassword(userRequest.Password);
+             var password = _todoDbContext.Users.Where(x => x.PasswordHash == passwordHash);*/
+            if (!_bCryptPasswordHash.VerifyPassword( userRequest.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+            /*if (password == null)
+            {
+                return BadRequest("Password not found");
+            }*/
+            
+            string token = _tokenGenerator.CreateToken(user);
+            return Ok(token);
+        }
+    }
+}
--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -25,7 +25,23 @@ internal class Program
         builder.Services.AddSingleton<BCryptPasswordHash>();
         builder.Services.AddSingleton<AccessTokenGenerator>();
         
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+     .AddJwtBearer(options =>
+     {
+         options.TokenValidationParameters = new TokenValidationParameters
+         {
+             ValidateIssuerSigningKey = true,
+             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                 .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+             ValidateIssuer = false,
+             ValidateAudience = false
+         };
+     });
+        builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
+            policy =>
+            {
+                policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+            }));
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
