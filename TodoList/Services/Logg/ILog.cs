0adaf86c340cb20186174b32001e67f15b494a5b
quynhhoaa <quynhhoa020305@gmail.com>
2023-03-13 22:53:05 +0700

    log

---
+++ TodoList/Services/Logg/ILog.cs
@@ -0,0 +1,11 @@
+﻿using TodoList.DTOs;
+using TodoList.Models;
+
+namespace TodoList.Services.Log
+{
+    public interface ILog
+    {
+        Task<User> Register(UserRequest userRequest);
+        Task<string> Login(UserRequest userRequest);
+    }
+}
