196fc2442ca749231329992ce31347fcbe9f010e
quynhhoaa <quynhhoa020305@gmail.com>
2023-03-23 00:46:31 +0700

    User

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -39,11 +39,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
              ValidateAudience = false
          };
      });
-        builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
-            policy =>
-            {
-                policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
-            }));
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
