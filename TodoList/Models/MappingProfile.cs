3c81e1e5484864e783e367a4fc13b204d8f8d21d
quynhhoaa <quynhhoa020305@gmail.com>
2023-03-27 13:06:16 +0700

    logs

---
+++ TodoList/Models/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TodoList.DTOs;
+
+namespace TodoList.Models
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, RegisterRequest>().ReverseMap();
+            CreateMap<User, UserResponse>().ReverseMap();
+        }
+    }
+}
